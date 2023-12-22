09863527bb81486ff54eb7cffcf65a0a898260d8
Danillo <danillodca@gmail.com>
2023-12-21 23:24:18 -0300

    atualizando linux

--- 04-JS-arrays/08-destructuring.js
+++ 04-JS-arrays/08-destructuring.js
@@ -1,5 +1,8 @@
 const numerosPares = [2, 4, 6, 8, 10];
 const numerosImpares = [3, 5, 7, 9, 11];
+console.log(numerosPares)
+numerosPares.push(12,14,16);
+console.log(numerosPares)
 
 console.log("\n destructuring  ############################################\n");
 const numeros = [...numerosPares, ...numerosImpares];
@@ -13,3 +16,19 @@ console.log (num1, num2);
 console.log("\ndeclarando  com destructuring #############################\n");
 const [num1b, num2b] = [1, 2]
 console.log (num1b, num2b);
+
+
+console.log("\ntestando com objetos #############################\n");
+const pessoa1={
+    nome: 'Danillo',
+    idade: 25
+}
+console.log(pessoa1)
+// const pessoa2 = pessoa1
+// const {pessoa2} = pessoa1
+let { idade } = pessoa1
+idade = pessoa1.idade + 1
+console.log(idade)
+pessoa1.idade = pessoa1.idade +1
+console.log(pessoa1.idade)
+console.log(pessoa1)
