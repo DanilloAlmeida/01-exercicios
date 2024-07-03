fea31822000a2483014c8cf087a07deaa8776a3f
DanilloAlmeida <danillodca@gmail.com>
2024-07-03 05:31:24 -0300

    att lnx

---
+++ 02-exercicios-javascript/05-teste.js
@@ -0,0 +1,14 @@
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+let valorString = '10';
+let valorNumero = 5;
+
+let resultado = valorString * valorNumero;
+console.log(resultado); // Saída: '105' (o número 5 foi convertido para string e concatenado com a string ‘10’)
---
+++ 04-JS-arrays/09-index2.js
@@ -0,0 +1,21 @@
+// const arrayVazia = [_,_];
+const arrayVazia = ['a','b',,, ];
+
+console.log(`conteúdo do array.: ${arrayVazia}`);
+console.log(`tamanho do array..: ${arrayVazia.length}`);
+console.log(`posição 0.........: ${arrayVazia[0]}`);
+console.log(`posição 1.........: ${arrayVazia[1]}`);
+console.log(`posição 2.........: ${arrayVazia[2]}`);
+console.log(`posição 3.........: ${arrayVazia[3]}`);
+console.log(`posição 4.........: ${arrayVazia[4]}`);
+console.log(`posição 5.........: ${arrayVazia[5]}`);
+arrayVazia.push('asdf')
+console.log("alterando com push\n\n")
+console.log(`conteúdo do array.: ${arrayVazia}`);
+console.log(`tamanho do array..: ${arrayVazia.length}`);
+console.log(`posição 0.........: ${arrayVazia[0]}`);
+console.log(`posição 1.........: ${arrayVazia[1]}`);
+console.log(`posição 2.........: ${arrayVazia[2]}`);
+console.log(`posição 3.........: ${arrayVazia[3]}`);
+console.log(`posição 4.........: ${arrayVazia[4]}`);
+console.log(`posição 5.........: ${arrayVazia[5]}`);
